ef43e2cc5671f67e4daec238cfd121ec91d8dd40
mcsec <mcsec@Lame>
2016-05-25 13:05:43 +0300

    add sid field

--- app/database/migrations/2016_05_25_075013_create_phone_numbers_table.php
+++ app/database/migrations/2016_05_25_075013_create_phone_numbers_table.php
@@ -17,6 +17,7 @@ class CreatePhoneNumbersTable extends Migration
 			$table->increments('id');
 			$table->string('country_code', 3);
 			$table->string('phone_number', 32);
+			$table->string('sid', 255);
 			$table->timestamps();
 		});
 	}
