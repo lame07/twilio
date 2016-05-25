802f3d1ad664ace516231c4bacb023fcd2eb7ff0
Maks Pavlenko <maks@maks.xyz>
2016-05-25 21:15:40 +0300

    add phone call registration

--- app/controllers/TwilioController.php
+++ app/controllers/TwilioController.php
@@ -5,6 +5,10 @@ class TwilioController extends BaseController
 
 	public function respond()
 	{
+		$phoneCall				 = new \models\PhoneCall();
+		$phoneCall->phone_number = Input::get('phone_number');
+		$phoneCall->save();
+
 		$response = new Services_Twilio_Twiml();
 		$response->say('Calling, wait a minute');
 		$response->dial(getenv('TWILIO_API_NUMBER'));
---
+++ app/models/PhoneCall.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PhoneCall extends Model
+{
+
+	protected $table = 'phone_calls';
+
+}
