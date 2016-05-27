b273e4d1658965464368340c25c17ec8d22b8d1e
Maks Pavlenko <maks@maks.xyz>
2016-05-27 10:30:40 +0300

    add alias to twilioRespond action too

--- app/routes.php
+++ app/routes.php
@@ -2,4 +2,4 @@
 
 Route::get('/', ['as' => 'indexPage', 'uses' => 'HomeController@showIndex']);
 Route::get('/callPage', ['as' => 'callPage', 'uses' => 'HomeController@showCallPage']);
-Route::get('/twilioRespond', 'TwilioController@respond');
+Route::get('/twilioRespond', ['as' => 'twilioRespond', 'uses' => 'TwilioController@respond']);
