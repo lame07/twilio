9fdcb0227d2f45f9af0f28d35261e2368b0e5130
Maks Pavlenko <maks@maks.xyz>
2016-05-27 10:50:40 +0300

    fix Services_Twilio usage as ServiceProvider

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -10,21 +10,39 @@ class HomeController extends BaseController
 
     public function showCallPage()
     {
-        $phoneNumber  = false;
-        $errorMessage = false;
+        if (!Input::has('country')) {
+            return Redirect::route('indexPage');
+        }
 
-        if (Input::has('country')) {
-            $twilioClient = new \Services_Twilio(getenv('TWILIO_API_SID'), getenv('TWILIO_API_TOKEN'));
+        $countryCode      = strtoupper(Input::get('country'));
+        $phoneNumberModel = \app\models\PhoneNumber::where('country_code', $countryCode)->first();
+        $phoneNumber      = false;
+        $errorMessage     = false;
 
-            $numberProvider = new \app\models\PhoneNumberProvider($twilioClient, Input::get('country'));
-
+        if ($phoneNumberModel === null) {
             try {
-                $phoneNumber = $numberProvider->getPhoneNumber()->phone_number;
+                $numbers = app(\Services_Twilio::class)->account->available_phone_numbers->getList($countryCode, 'Local');
+                $number  = app(\Services_Twilio::class)->account->incoming_phone_numbers->create(array(
+                    "FriendlyName" => "{$countryCode} Number",
+                    "VoiceUrl"     => URL::route('twilioRespond'),
+                    "PhoneNumber"  => $numbers->available_phone_numbers[0]->phone_number,
+                    "VoiceMethod"  => "GET"
+                ));
+
+                if ($number) {
+                    $phoneNumberModel               = new \app\models\PhoneNumber();
+                    $phoneNumberModel->phone_number = $number->phone_number;
+                    $phoneNumberModel->sid          = $number->sid;
+                    $phoneNumberModel->country_code = $countryCode;
+                    $phoneNumberModel->save();
+                }
             } catch (\Services_Twilio_RestException $e) {
                 $errorMessage = $e->getMessage();
             }
-        } else {
-            return Redirect::route('indexPage');
+        }
+
+        if ($phoneNumberModel instanceof \app\models\PhoneNumber) {
+            $phoneNumber = $phoneNumberModel->phone_number;
         }
 
         return View::make('call_page')->with('phoneNumber', $phoneNumber)->with('errorMessage', $errorMessage);
